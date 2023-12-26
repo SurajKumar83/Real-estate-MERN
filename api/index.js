f6afd846593b6c7357396db61aed896df5bcf6f3
Suraj Kumar <surajkumar834069@gmail.com>
2023-12-26 15:59:54 +0530

    First commit

---
+++ api/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+const app = express();
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => console.log(err));
+
+app.listen(3000, () => {
+  console.log("Server is running on the port 3000");
+});
---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/sign-in" element={<Signin />}></Route>
+        <Route path="/sign-up" element={<Signup />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
