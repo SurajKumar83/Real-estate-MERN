e2f52bd424a23cbfcdca937dcc2562c4644564a0
Suraj Kumar <surajkumar834069@gmail.com>
2024-01-09 20:59:46 +0530

    add onChange and onSubmit functionality in  search page

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -65,22 +65,22 @@ export const getListings = async (req, res, next) => {
     const limit = parseInt(req.query.limit) || 9;
     const startIndex = parseInt(req.query.startIndex) || 0;
     let offer = req.query.offer;
-    if (offer === undefined || offer === false) {
+    if (offer === undefined || offer === "false") {
       offer = { $in: [false, true] }; // this will set the offer to intially useless term because initially we have to show both offer as well as not offered
     }
     let furnished = req.query.furnished;
-    if (furnished === undefined || furnished === false) {
+    if (furnished === undefined || furnished === "false") {
       furnished = { $in: [false, true] };
     }
     let parking = req.query.parking;
-    if (parking === undefined || parking === false) {
+    if (parking === undefined || parking === "false") {
       parking = { $in: [false, true] };
     }
     let type = req.query.type;
     if (type === undefined || type === "all") {
       type = { $in: ["sale", "rent"] };
     }
-    const searchTerm = req.query.searhTerm || "";
+    const searchTerm = req.query.searchTerm || "";
     const sort = req.query.sort || "createdAt";
     const order = req.query.order || "desc";
 
