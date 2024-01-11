38755e23f8f1fd7a0eb22640f9d68bdfc803de1a
Suraj Kumar <surajkumar834069@gmail.com>
2024-01-11 22:17:40 +0530

    Complete About Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
-
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Suraj Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Welcome to Suraj Estate, where we take pride in transforming dreams into
+        reality through our dedicated real estate solutions. With a rich legacy
+        of excellence spanning over two decades, Suraj Estate has been a trusted
+        name in the industry, consistently delivering exceptional properties and
+        unparalleled service. Our commitment to quality, innovation, and
+        customer satisfaction sets us apart, making us a preferred choice for
+        individuals and businesses seeking premium real estate options.
+      </p>
+      <p className="mb-4 text-slate-700">
+        At Suraj Estate, we believe in crafting not just buildings, but
+        lifestyles. Our diverse portfolio includes residential, commercial, and
+        mixed-use developments, each designed with meticulous attention to
+        detail and a focus on sustainability. Whether you are looking for a cozy
+        home, a strategic business location, or an investment opportunity, Suraj
+        Estate offers a range of choices to meet your unique needs. Our team of
+        seasoned professionals is dedicated to guiding you through every step of
+        the real estate journey, ensuring a seamless and rewarding experience.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Beyond bricks and mortar, Suraj Estate is deeply committed to
+        contributing to the communities we serve. Through responsible and
+        ethical business practices, we aim to create lasting value for our
+        clients, partners, and the society at large. Explore the world of Suraj
+        Estate, where a commitment to excellence, integrity, and innovation
+        converges to redefine the possibilities of modern living and business.
+      </p>
+    </div>
+  );
+};
 
-export default About
+export default About;
